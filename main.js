539b6a9947602c7e4d003d20c440615adf5d7fda
Linn2punkt0 <linn.johansson7@hotmail.com>
2018-12-18 15:57:56 +0100

    Organised directory

--- main.js
+++ main.js
@@ -2,10 +2,12 @@ let menuIsOpen = false;
 
 // Burger-animation
 const navIcon = document.querySelector("#nav-icon3");
+const navbar = document.querySelector(".navbar");
 const menuSelector = document.querySelector(".menu-selector");
 navIcon.addEventListener("click", () => {
   navIcon.classList.toggle("open");
   menuSelector.classList.toggle("display");
+  navbar.classList.toggle("hide");
   menuIsOpen = !menuIsOpen;
 });
 
@@ -14,6 +16,7 @@ const menuLinks = menuSelector.querySelectorAll(".goTo");
 function closeMenu() {
   navIcon.classList.toggle("open");
   menuSelector.classList.remove("display");
+  navbar.classList.remove("hide");
   menuIsOpen = !menuIsOpen;
 }
 
